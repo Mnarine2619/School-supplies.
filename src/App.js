d82a6963a67ee11963c94ab71082666cde8815df
Mnarine2619 <mnarine2619@richmondhillhs.org>
2021-03-19 05:42:52 +0000

    Final

--- src/App.js
+++ src/App.js
@@ -7,16 +7,27 @@ import Summary from './Components/Summary.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+        cart: [] ,
+        total: 0
+    }
   }
-  
+ buy = (thing, price) =>  {
+this.ListeningStateChangedEvent((state) => {
+    state.cart.push(thing)
+    state.total += parseFloat(price)
+    return state
+})
+
+ }
   render() {
     return (
       <div className="App">
         <Hero />
-        <Product type="Laptop" price="999.00" />
-        <Product type="Mechanical Pencil" price="0.25" />
-        <Product type="College Ruled Loose Leaf" price="2.75" />
+        <Product type="Laptop" price="999.00" buy = {this.boy}/>
+        <Product type="Mechanical Pencil" price="0.25" buy = {this.boy}/>
+        <Product type="College Ruled Loose Leaf" price="2.75" buy = {this.boy}/>
+        <Product type ="Eraser" price = "8.00" buy ={this.boy}/>
         <Summary globalState={this.state} />
       </div>
     );
